d0b2cfea49afcb05aef5791c20a35712bcd54b76
Arthi Vijayakumar <av439@cornell.edu>
2023-10-22 18:33:13 -0400

    Implemented the position.mli

--- lib/position.mli
+++ lib/position.mli
@@ -1,1 +1,9 @@
+module Positions_map : Map.S with type key = int * int
 
+module type PositionOfPieces = sig
+  val buildRowOfMap :
+    'a Positions_map.t -> int -> int -> int -> 'a Positions_map.t
+
+  val buildEmptyMap : 'a Positions_map.t -> int -> int -> int
+  val placePiece : int -> int -> int -> 'a Positions_map.t
+end
