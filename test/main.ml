ce0903d2630e8771772984a68b8916178c31ffd4
Mary Yuan <cy452@cornell.edu>
2023-12-05 14:55:36 -0500

    Initialized the test suite.

---
+++ test/main.ml
@@ -0,0 +1,15 @@
+open OUnit2
+open Othello
+
+let pp_list pp_elt lst =
+  let pp_elts lst =
+    let rec loop n acc = function
+      | [] -> acc
+      | [ h ] -> acc ^ pp_elt h
+      | h1 :: (h2 :: t as t') ->
+          if n = 100 then acc ^ "..." (* stop printing long list *)
+          else loop (n + 1) (acc ^ pp_elt h1 ^ "; ") t'
+    in
+    loop 0 "" lst
+  in
+  Printf.sprintf "[%s]" (pp_elts lst)
