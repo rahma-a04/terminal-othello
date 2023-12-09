998885ace3e98a2e158158e43771f6db9f1d61e4
Mary Yuan <cy452@cornell.edu>
2023-12-09 17:07:45 -0500

    wrote the author.ml and author.mli files

---
+++ lib/author.mli
@@ -0,0 +1,17 @@
+(************************************************************
+   Copyright (C) 2023 Mary Yuan, Arthi Vijayakumar, Om Deshmukh, and Rahma 
+   Abdullah.
+   Created by Mary Yuan, Arthi Vijayakumar, Om Deshmukh, and Rahma Abdullah.
+   You may not redistribute this game, distribute any derivatives,
+   or use it for commercial purposes.
+ ************************************************************)
+
+(** Othello
+
+    @author Mary Yuan (cy452)
+    @author Arthi Vijayakumar (av439)
+    @author Om Deshmukh (od66)
+    @author Rahma Abdullah (ra567) *)
+
+val hours_worked : int
+(** [hours_worked] is the number of hours we worked on this game. *)
