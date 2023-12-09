8a6a854bd5a74dd66ac3953fae43fd28a3b9223f
Om Deshmukh <od66@cornell.edu>
2023-12-09 17:24:31 -0500

    Fully fleshed out godmode functionality, takes addition preference to edges/corners & updated evaluate_move

--- lib/computerPlayer.ml
+++ lib/computerPlayer.ml
@@ -33,8 +33,7 @@ let generateMoveMedium (valid_moves : (int * int) list) (board : Board.board)
   | [] -> raise (Invalid_argument "no more moves :(")
   | (_, best_move) :: _ -> best_move
 
-let evaluate_board board color =
-  let score = ref 0 in
+let evaluate_move (x, y) color =
   let weight_matrix =
     [|
       [| 4; -3; 2; 2; 2; 2; -3; 4 |];
@@ -47,24 +46,15 @@ let evaluate_board board color =
       [| 4; -3; 2; 2; 2; 2; -3; 4 |];
     |]
   in
-  for i = 0 to 7 do
-    for j = 0 to 7 do
-      match Board.get_element i j board with
-      | piece when piece = color -> score := !score + weight_matrix.(i).(j)
-      | piece when piece <> Board.Empty ->
-          score := !score - weight_matrix.(i).(j)
-      | _ -> ()
-    done
-  done;
-  !score
+  weight_matrix.(x).(y)
 
 let generateMoveHard (valid_moves : (int * int) list) (board : Board.board)
     (color : Board.piece) : int * int =
   let simulate_move (x, y) =
+    let move_score = evaluate_move (x, y) color in
     let simulated_board = Board.place_and_flip_pieces x y color board in
-    let board_score = evaluate_board simulated_board color in
     let flipped_count = count_flipped_pieces board simulated_board color in
-    (board_score, flipped_count, (x, y))
+    (move_score, flipped_count, (x, y))
   in
   let moves_with_scores = List.map simulate_move valid_moves in
   let compare (score1, flips1, _) (score2, flips2, _) =
@@ -75,12 +65,41 @@ let generateMoveHard (valid_moves : (int * int) list) (board : Board.board)
   | [] -> raise (Invalid_argument "no more moves :(")
   | (_, _, best_move) :: _ -> best_move
 
+let evaluate_move_extreme (x, y) color board =
+  let weight_matrix =
+    [|
+      [| 5; -3; 3; 2; 2; 3; -3; 5 |];
+      [| -3; -4; -1; -1; -1; -1; -4; -3 |];
+      [| 3; -1; 1; 0; 0; 1; -1; 3 |];
+      [| 2; -1; 0; 1; 1; 0; -1; 2 |];
+      [| 2; -1; 0; 1; 1; 0; -1; 2 |];
+      [| 3; -1; 1; 0; 0; 1; -1; 3 |];
+      [| -3; -4; -1; -1; -1; -1; -4; -3 |];
+      [| 5; -3; 3; 2; 2; 3; -3; 5 |];
+    |]
+  in
+  let move_weight = if x >= 0 && y >= 0 then weight_matrix.(x).(y) else 0 in
+  let simulated_board = Board.place_and_flip_pieces x y color board in
+  let flipped_count = count_flipped_pieces board simulated_board color in
+  let is_edge_or_corner (x, y) =
+    (x = 0 || x = 7 || y = 0 || y = 7)
+    || (x = 0 && y = 0)
+    || (x = 7 && y = 7)
+    || (x = 0 && y = 7)
+    || (x = 7 && y = 0)
+  in
+  let additional_edge_bonus =
+    if is_edge_or_corner (x, y) && flipped_count > 0 then 2 else 0
+  in
+  (move_weight + additional_edge_bonus, flipped_count)
+
+(** Generate move for god mode AI using Minimax w/ Alpha-Beta Pruning *)
 let rec alpha_beta_prune board depth alpha beta color maximizingPlayer =
   if depth = 0 || Board.is_board_filled board then
-    (evaluate_board board color, (-1, -1))
+    evaluate_move_extreme (-1, -1) color board
   else
     let valid_moves = Board.find_all_valid_moves color board in
-    if List.length valid_moves = 0 then (evaluate_board board color, (-1, -1))
+    if valid_moves = [] then evaluate_move_extreme (-1, -1) color board
     else
       let best_value =
         ref (if maximizingPlayer then Int.min_int else Int.max_int)
@@ -89,31 +108,26 @@ let rec alpha_beta_prune board depth alpha beta color maximizingPlayer =
       List.iter
         (fun (x, y) ->
           let new_board = Board.place_and_flip_pieces x y color board in
-          let eval, _ =
-            alpha_beta_prune new_board (depth - 1) alpha beta
-              (match color with
-              | White -> Black
-              | Black -> White
-              | Empty -> failwith "bruh")
-              (not maximizingPlayer)
+          let eval, flipped_count =
+            evaluate_move_extreme (x, y) color new_board
           in
+          let total_score = eval + flipped_count in
+          (* Combining two scoring criteria, weight matrix & flip count*)
           if
-            (maximizingPlayer && eval > !best_value)
-            || ((not maximizingPlayer) && eval < !best_value)
+            (maximizingPlayer && total_score > !best_value)
+            || ((not maximizingPlayer) && total_score < !best_value)
           then (
-            best_value := eval;
+            best_value := total_score;
             best_move := (x, y));
-          if maximizingPlayer then alpha := max !alpha eval
-          else beta := min !beta eval;
+          if maximizingPlayer then alpha := max !alpha total_score
+          else beta := min !beta total_score;
           if !beta <= !alpha then ())
         valid_moves;
-      (!best_value, !best_move)
+      !best_move
 
-(** Generate move for god mode AI using Minimax w/ Alpha-Beta Pruning *)
 let generateMoveExtreme (valid_moves : (int * int) list) board color =
-  let depth = 5 in
-  (*Increase/decrease the depth to increase/decrease difficulty*)
-  let _, best_move =
+  let depth = 5 (* Adjust depth for difficulty *) in
+  let x, y =
     alpha_beta_prune board depth (ref Int.min_int) (ref Int.max_int) color true
   in
-  best_move
+  (x, y)
