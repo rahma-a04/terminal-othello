c464b2828396d9f238159f96d42d846eed16ee84
Om Deshmukh <od66@cornell.edu>
2023-12-08 23:58:59 -0500

    Implemented hard AI

--- lib/computerPlayer.ml
+++ lib/computerPlayer.ml
@@ -1,24 +1,26 @@
-let rec generateMoveHelper (possibleMoves : (int * int) list) (v : int)
+let rec generateMoveHelper (valid_moves : (int * int) list) (v : int)
     (currentNum : int) : int * int =
-  match possibleMoves with
+  match valid_moves with
   | (x, y) :: tail ->
       if currentNum <> v then generateMoveHelper tail v (currentNum + 1)
       else (x, y)
   | [] -> raise (Invalid_argument "no more moves :(")
 
-let generateMove (possibleMoves : (int * int) list) : int * int =
-  if List.length possibleMoves = 0 then
+let generateMoveEasy (valid_moves : (int * int) list) (board : Board.board)
+    (color : Board.piece) : int * int =
+  if List.length valid_moves = 0 then
     raise (Invalid_argument "error, possible move list is 0")
   else
-    let movePos = Random.int (List.length possibleMoves) in
-    generateMoveHelper possibleMoves movePos 0
+    let movePos = Random.int (List.length valid_moves) in
+    generateMoveHelper valid_moves movePos 0
 
 let count_flipped_pieces original_board new_board color =
   let original_count = Board.count_pieces original_board color in
   let new_count = Board.count_pieces new_board color in
   new_count - original_count
 
-let generateMoveMedium valid_moves board color =
+let generateMoveMedium (valid_moves : (int * int) list) (board : Board.board)
+    (color : Board.piece) : int * int =
   (*let valid_moves = Board.find_all_valid_moves color board in*)
   let simulate_move (x, y) =
     let simulated_board = Board.place_and_flip_pieces x y color board in
