96d62462717f51de724911c9d19ec45ae2271ee7
Arthi Vijayakumar <av439@cornell.edu>
2023-12-10 14:29:01 -0500

    Added specs for UI.ml

--- lib/ui.ml
+++ lib/ui.ml
@@ -2,29 +2,38 @@ open Board
 
 exception End_game
 
+(* Describes the type game state, which is composed of a board, the particular
+   move, and a player*)
 type game_state = {
   board : board;
   move_num : int;
   player : piece;
 }
 
+(* Defines type game to be a list of game_state*)
 type game = game_state list
 
+(* Creates a new game *)
 let new_game =
   let state = { board = empty_board; move_num = 0; player = Black } in
   [ state ]
 
+(* Prints the player as Black or White*)
 let print_player p =
   match p with
   | Black -> "Black"
   | White -> "White"
   | _ -> failwith "Impossible"
 
+(*Matches the input with a game and returns the board. Raises: invalid_argument
+  if the input is not a game*)
 let board_of_game g =
   match g with
   | { board = b; move_num = _; player = _ } :: t -> b
   | [] -> failwith "Invalid input: game doesn't exist"
 
+(* Matches the input with a game and returns the player. Raises:
+   invalid_argument if the input is not a game*)
 let player_of_game g =
   match g with
   | { board = _; move_num = _; player = p } :: t -> p
@@ -53,7 +62,8 @@ let update (row : int) (col : int) (game : game) : game =
     end
   | [] -> failwith "Invalid input: game doesn't exist"
 
-(** Skips the current player's turn in the game. *)
+(** Skips the current player's turn in the game. Raises: invalid_argument if a
+    game is not passed in*)
 let skip_turn (game : game) =
   match game with
   | { board = b; move_num = n; player = p } :: t ->
@@ -69,12 +79,14 @@ let rec trim x lst =
     | h :: t -> trim (x - 1) t
     | [] -> []
 
+(* Moves a game back to a previous game state *)
 let revert (game : game) n =
   if n < 0 || n > List.length game - 1 then
     raise
       (Failure (string_of_int n ^ " is outside the game's valid move range."))
   else trim (List.length game - (n + 1)) game
 
+(* Prints the current game state *)
 let print_curr_state (state : game_state) =
   match state with
   | { board = b; move_num = n; player = p } ->
@@ -83,11 +95,13 @@ let print_curr_state (state : game_state) =
       print_board b;
       print_endline ("[PLAYER: " ^ code ^ " " ^ print_player p ^ "]")
 
+(* Prints every state of a game to print a game*)
 let print_current_game (game : game) =
   match game with
   | h :: t -> print_curr_state h
   | [] -> failwith "Impossible"
 
+(* A helper function to print_previous_games that prints every state of a game*)
 let rec print_prev_helper = function
   | h :: t ->
       print_curr_state h;
@@ -98,6 +112,7 @@ let rec print_prev_helper = function
       print_prev_helper t
   | [] -> print_newline ()
 
+(* A function that prints the game history and calls print_prev_helper *)
 let print_previous_games (game : game) =
   print_newline ();
   print_endline
